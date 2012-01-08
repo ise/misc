db7e43060f29c9bfcc7c5522075aa6882227395d
Daisuke Ise <isedaisuke@gmail.com>
2012-01-08 21:55:12 +0900

    closure

--- scala/cup/FileMatcher.scala
+++ scala/cup/FileMatcher.scala
@@ -1,12 +1,12 @@
 object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
-  private def filesMatching(query: String, matcher: (String, String) => Boolean) =
-    for (file <- filesHere; if matcher(file.getName, query))
+  private def filesMatching(matcher: String => Boolean) =
+    for (file <- filesHere; if matcher(file.getName))
       yield file
   def filesEnding(query: String) = 
-    filesMatching(query, _.endsWith(_))
+    filesMatching(_.endsWith(query))
   def filesContaining(query: String) =
-    filesMatching(query, _.contains(_))
+    filesMatching(_.contains(query))
 }
 
 object Main extends App {
